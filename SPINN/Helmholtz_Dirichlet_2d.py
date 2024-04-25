7a6d79fc09a4e2f830821cb77a9e985cb05d925c
bonneted <bonneteymard.damien@gmail.com>
2024-04-25 14:49:56 +0200

    add spinn Helmhotz examples

---
+++ SPINN/Helmholtz_Dirichlet_2d.py
@@ -0,0 +1,121 @@
+"""Backend supported: tensorflow.compat.v1, tensorflow, pytorch, paddle"""
+import deepxde as dde
+import numpy as np
+
+# General parameters
+n = 2
+precision_train = 10
+precision_test = 30
+hard_constraint = True
+weights = 200  # if hard_constraint == False
+iterations = 10000
+parameters = [1e-3, 3, 150, "sin"]
+net_type = "spinn"
+
+if net_type == "pinn":
+    dde.config.set_default_autodiff("forward")
+
+
+# Define functions
+sin = dde.backend.sin
+stack = dde.backend.stack
+
+if dde.backend.backend_name == "jax":
+    import jax.numpy as jnp
+
+learning_rate, num_dense_layers, num_dense_nodes, activation = parameters
+
+
+def pde(x, y):
+
+    if net_type == "spinn":
+        x_mesh = [x_.ravel() for x_ in jnp.meshgrid(x[:, 0], x[:, 1], indexing="ij")]
+        x = stack(x_mesh, axis=-1)
+
+    dy_xx = dde.grad.hessian(y, x, i=0, j=0)
+    dy_yy = dde.grad.hessian(y, x, i=1, j=1)
+
+    if dde.backend.backend_name == "jax":
+        y = y[0]
+        dy_xx = dy_xx[0]
+        dy_yy = dy_yy[0]
+
+    f = k0 ** 2 * sin(k0 * x[:, 0:1]) * sin(k0 * x[:, 1:2])
+    return -dy_xx - dy_yy - k0 ** 2 * y - f
+
+
+def func(x):
+    if net_type == "spinn":
+        x_mesh = [x_.ravel() for x_ in jnp.meshgrid(x[:, 0], x[:, 1], indexing="ij")]
+        x = jnp.stack(x_mesh, axis=-1)
+    return np.sin(k0 * x[:, 0:1]) * np.sin(k0 * x[:, 1:2])
+
+
+def transform(x, y):
+    # print(x.shape, y.shape)
+    if net_type == "spinn":
+        x_mesh = [x_.ravel() for x_ in jnp.meshgrid(x[:, 0], x[:, 1], indexing="ij")]
+        x = jnp.stack(x_mesh, axis=-1)
+    res = x[:, 0:1] * (1 - x[:, 0:1]) * x[:, 1:2] * (1 - x[:, 1:2])
+    # out = res * y
+    # print(out.shape, out.reshape(-1).shape)
+    return res * y
+
+
+def boundary(_, on_boundary):
+    return on_boundary
+
+
+geom = dde.geometry.Rectangle([0, 0], [1, 1])
+k0 = 2 * np.pi * n
+wave_len = 1 / n
+
+hx_train = wave_len / precision_train
+nx_train = int(1 / hx_train)
+
+hx_test = wave_len / precision_test
+nx_test = int(1 / hx_test)
+
+if hard_constraint == True:
+    bc = []
+else:
+    bc = dde.icbc.DirichletBC(geom, lambda x: 0, boundary)
+
+
+data = dde.data.PDE(
+    geom,
+    pde,
+    bc,
+    num_domain=nx_train if net_type == "spinn" else nx_train ** 2,
+    num_boundary=0 if hard_constraint else nx_train * 4, 
+    solution=func,
+    num_test=nx_test if net_type == "spinn" else nx_test ** 2,
+    is_SPINN=net_type == "spinn",
+)
+
+if net_type == "spinn":
+    net = dde.nn.SPINN([2] + [num_dense_nodes] * num_dense_layers + [36, 1], activation, "Glorot uniform", "mlp")
+else:
+    net = dde.nn.FNN(
+        [2] + [num_dense_nodes] * num_dense_layers + [1], activation, "Glorot uniform"
+    )
+
+if hard_constraint == True:
+    net.apply_output_transform(transform)
+
+model = dde.Model(data, net)
+
+if hard_constraint == True:
+    model.compile("adam", lr=learning_rate, metrics=["l2 relative error"])
+else:
+    loss_weights = [1, weights]
+    model.compile(
+        "adam",
+        lr=learning_rate,
+        metrics=["l2 relative error"],
+        loss_weights=loss_weights,
+    )
+
+
+losshistory, train_state = model.train(iterations=iterations, display_every=100)
+# dde.saveplot(losshistory, train_state, issave=True, isplot=True)
